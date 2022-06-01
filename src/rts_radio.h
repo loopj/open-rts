f1b48f690f18822b2ff09250db9829147ea4dd14
James Smith <james@loopj.com>
2022-06-01 13:00:15 -0700

    Document rts_radio and re-document rts_remote_store_memory

--- src/rts_radio.h
+++ src/rts_radio.h
@@ -5,6 +5,9 @@
 #include "radio/spi_module.h"
 #include "radio/sx1278.h"
 
+/**
+ * The transceiver mode to enter
+ */
 enum rts_radio_mode {
     RTS_RADIO_MODE_STANDBY,
     RTS_RADIO_MODE_RECEIVE,
@@ -16,6 +19,9 @@ enum rts_radio_module {
     RTS_RADIO_MODULE_SX1278,
 };
 
+/**
+ * Abstraction layer which configures radio modules for RTS.
+ */
 struct rts_radio {
     enum rts_radio_module type;
     union {
@@ -28,12 +34,39 @@ struct rts_radio {
 extern "C" {
 #endif
 
+/**
+ * Initialize an rts_radio to use an SX1231/RFM69 radio module.
+ *
+ * @relates rts_radio
+ *
+ * @param radio the rts_radio struct to initialize
+ * @param spi the spi module to communicate with
+ * @param use_pa_boost is this module using PA_BOOST for transmission
+ */
 void rts_radio_init_rfm69(struct rts_radio *radio, struct spi_module *spi,
                           bool use_pa_boost);
 
+/**
+ * Initialize an rts_radio to use an SX1278/RFM96 radio module.
+ *
+ * @relates rts_radio
+ *
+ * @param radio the rts_radio struct to initialize
+ * @param spi the spi module to communicate with
+ * @param use_pa_boost is this module using PA_BOOST for transmission
+ */
 void rts_radio_init_sx1278(struct rts_radio *radio, struct spi_module *spi,
                            bool use_pa_boost);
 
+/**
+ * Set the radio mode for the specified rts_radio module, eg. standby, recevie,
+ * or transmit.
+ *
+ * @relates rts_radio
+ *
+ * @param radio the rts_radio struct to set the mode for
+ * @param mode the rts_radio_mode to enter
+ */
 void rts_radio_set_mode(struct rts_radio *radio, enum rts_radio_mode mode);
 
 #ifdef __cplusplus
--- src/rts_remote_store_memory.h
+++ src/rts_remote_store_memory.h
@@ -7,13 +7,43 @@
 extern "C" {
 #endif
 
+/**
+ * Initialize an rts_remote_store which stores remotes and rolling codes in
+ * memory.
+ *
+ * @relates rts_remote_store
+ *
+ * @param store the rts_remote_store struct to initialize
+ */
 void rts_remote_store_init_memory(struct rts_remote_store *store);
 
+/**
+ * Free any memory allocated by rts_remote_store_init_memory.
+ *
+ * @relates rts_remote_store
+ *
+ * @param store the rts_remote_store struct to free
+ */
 void rts_remote_store_free_memory(struct rts_remote_store *store);
 
+/**
+ * Initialize an rts_remote_store which stores remotes and rolling codes in
+ * a memory-mapped file on POSIX systems.
+ *
+ * @relates rts_remote_store
+ *
+ * @param store the rts_remote_store struct to initialize
+ */
 void rts_remote_store_init_mmap(struct rts_remote_store *store,
                                 const char *filename);
 
+/**
+ * Free any memory and file handles allocated by rts_remote_store_init_mmap.
+ *
+ * @relates rts_remote_store
+ *
+ * @param store the rts_remote_store struct to free
+ */
 void rts_remote_store_free_mmap(struct rts_remote_store *store);
 
 #ifdef __cplusplus
