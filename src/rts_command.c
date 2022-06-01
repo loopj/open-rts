5c885163dc777dcfe28c32220a75a33f39389a0a
James Smith <james@loopj.com>
2022-06-01 15:39:09 -0700

    rts_command_to_string

---
+++ src/rts_command.c
@@ -0,0 +1,18 @@
+#include "rts_command.h"
+
+const char *rts_command_to_string(enum rts_command command)
+{
+    switch(command) {
+        case RTS_COMMAND_MY: return "My";
+        case RTS_COMMAND_UP: return "Up";
+        case RTS_COMMAND_MY_UP: return "My+Up";
+        case RTS_COMMAND_DOWN: return "Down";
+        case RTS_COMMAND_MY_DOWN: return "My+Down";
+        case RTS_COMMAND_UP_DOWN: return "Up+Down";
+        case RTS_COMMAND_MY_UP_DOWN: return "My+Up+Down";
+        case RTS_COMMAND_PROG: return "Prog";
+        case RTS_COMMAND_SUN_FLAG: return "Sun+Flag";
+        case RTS_COMMAND_FLAG: return "Flag";
+        default: return "Unknown";
+    }
+}
--- src/rts_command.h
+++ src/rts_command.h
@@ -1,7 +1,7 @@
+/// @file
+
 #ifndef RTS_COMMAND_H
 #define RTS_COMMAND_H
-
-/// @file
 
 /**
  * Command sent by a remote, or received by a receiver as part of
@@ -22,5 +22,7 @@ enum rts_command {
     RTS_COMMAND_SUN_FLAG   = 0x9, ///< Sun and Flag button pressed (Soliris)
     RTS_COMMAND_FLAG       = 0xA, ///< Flag button pressed (Soliris)
 };
+
+const char *rts_command_to_string(enum rts_command command);
 
 #endif // RTS_COMMAND_H
