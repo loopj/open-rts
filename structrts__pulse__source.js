013792bc6b0f769b72b4683727d558d0d24b76ea
loopj <loopj@users.noreply.github.com>
2022-06-02 00:13:01 +0000

    deploy: 1d1c8fbebd3d73bc2cb291be90807fef9769a4da

--- structrts__pulse__source.js
+++ structrts__pulse__source.js
@@ -4,6 +4,7 @@ var structrts__pulse__source =
     [ "rts_pulse_source_disable", "structrts__pulse__source.html#a45cfff3972b1a810712084835660f78c", null ],
     [ "rts_pulse_source_update", "structrts__pulse__source.html#a331d02f4adc1a5adeffeadfe03c0c9a0", null ],
     [ "rts_pulse_source_attach", "structrts__pulse__source.html#af29fc3e4b2e446040c435f9400177934", null ],
+    [ "rts_pulse_source_close", "structrts__pulse__source.html#aaf1a44ffce736c880c87dac96b0e661d", null ],
     [ "rts_pulse_source_init_espidf_gpio", "structrts__pulse__source.html#a0739f646f14a3628a130e80120dec3fe", null ],
     [ "rts_pulse_source_init_gpiod", "structrts__pulse__source.html#aa898334853d0f0180a97fa4c093bff9b", null ]
 ];
--- structrts__remote__store.js
+++ structrts__remote__store.js
@@ -6,9 +6,8 @@ var structrts__remote__store =
     [ "rts_remote_store_known_remote", "structrts__remote__store.html#a6ecf66e04d8462b100b142cecc6a338d", null ],
     [ "rts_remote_store_forget", "structrts__remote__store.html#a93d26435ee32bda67ad0958cd08bfbfb", null ],
     [ "rts_remote_store_clear", "structrts__remote__store.html#aba1b041180b813756605331bba63a006", null ],
+    [ "rts_remote_store_close", "structrts__remote__store.html#a55de60ac69a0b8576e52214fc51d22fe", null ],
     [ "rts_remote_store_init_memory", "structrts__remote__store.html#a52529ac5fcf5001a80dd19e3d490bf47", null ],
-    [ "rts_remote_store_free_memory", "structrts__remote__store.html#acac2ae695f971a8f5e9fac055421cb74", null ],
     [ "rts_remote_store_init_mmap", "structrts__remote__store.html#a947e08504299d8fe5812ec0c18a699c3", null ],
-    [ "rts_remote_store_free_mmap", "structrts__remote__store.html#ab0816a9dbb5e3383a5d87878948d1f76", null ],
     [ "rts_remote_store_init_nvs", "structrts__remote__store.html#afefe1fed614ea5110fed6eb69d2d9cdd", null ]
 ];
