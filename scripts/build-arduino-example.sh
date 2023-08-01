4a32620f23c302c628108b262a38a92924003ed4
James Smith <james@loopj.com>
2023-08-01 13:51:42 -0700

    Allow passing of port to command line

--- scripts/build-arduino-example.sh
+++ scripts/build-arduino-example.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This script requires
+# - Bash >= 4.0
+# - Arduino CLI
+
+# brew install bash arduino-cli
+
+# arduino-cli config init
+# arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+# arduino-cli core update-index
+# arduino-cli core install
+# arduino-cli lib install U8g2
 
 declare -A board_fqbns=()
 declare -A board_flags=()
-declare -A board_ports=()
 
 # SparkFun LoRa Gateway (w/RFM69 module)
 board_fqbns["sparkfun"]="esp32:esp32:sparkfun_lora_gateway_1-channel"
-board_ports["sparkfun"]="/dev/ttyUSB0"
 board_flags["sparkfun"]="
     -DOPENRTS_BOARD_SPARKFUN_LORA_GATEWAY
     -DOPENRTS_BUTTON_2=23
@@ -16,14 +26,12 @@ board_flags["sparkfun"]="
 
 # LILYGO TTGO LoRa32 (V2.1)
 board_fqbns["ttgo"]="esp32:esp32:ttgo-lora32:Revision=TTGO_LoRa32_v21new"
-board_ports["ttgo"]="/dev/ttyACM0"
 board_flags["ttgo"]="
     -DOPENRTS_BOARD_TTGO_LORA32_V21
 "
 
 # Heltec WiFi LoRa 32 (V2)
 board_fqbns["heltec"]="esp32:esp32:heltec_wifi_lora_32_V2"
-board_ports["heltec"]="/dev/ttyUSB0"
 board_flags["heltec"]="
     -DOPENRTS_BOARD_HELTEC_WIFI_LORA_32_V2
 "
@@ -32,7 +40,6 @@ board_flags["heltec"]="
 # with Adafruit Radio FeatherWing RFM69HCW
 # RST=A, CS=B, DIO2=25
 board_fqbns["featheresp32"]="esp32:esp32:featheresp32"
-board_ports["featheresp32"]="/dev/ttyUSB0"
 board_flags["featheresp32"]="
     -DOPENRTS_RADIO_TYPE_RFM69
     -DOPENRTS_RADIO_CS=33
@@ -47,7 +54,6 @@ board_flags["featheresp32"]="
 
 # Adafruit Feather 32u4 RFM69HCW
 board_fqbns["feather32u4"]="adafruit:avr:feather32u4"
-board_ports["feather32u4"]="/dev/ttyACM0"
 board_flags["feather32u4"]="
     -DOPENRTS_BOARD_ADAFRUIT_32U4_RFM69
     -DOPENRTS_BUTTON_1=9
@@ -58,7 +64,6 @@ board_flags["feather32u4"]="
 
 # Adafruit Feather M0 RFM69HCW
 board_fqbns["featherm0"]="adafruit:samd:adafruit_feather_m0"
-board_ports["featherm0"]="/dev/ttyACM0"
 board_flags["featherm0"]="
     -DOPENRTS_BOARD_ADAFRUIT_M0_RFM69
     -DOPENRTS_BUTTON_1=9
@@ -74,9 +79,9 @@ for i in ${example_paths[@]}; do
     example_names+=(${i##*/})
 done
 
-# Check example and board are passed
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <example> <board>" 1>&2
+# Check example, board, and port are passed
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <example> <board> <port>" 1>&2
     exit 1
 fi
 
@@ -104,7 +109,7 @@ arduino-cli compile \
     --warnings all\
     --dump-profile \
     --upload \
-    --port ${board_ports[$2]} \
+    --port $3 \
 	--fqbn ${board_fqbns[$2]} \
 	--build-property "compiler.cpp.extra_flags=${board_flags[$2]}" \
 	--library="$PWD" \
